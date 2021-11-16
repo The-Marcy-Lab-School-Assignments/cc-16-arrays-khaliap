de6304a0ec9eb0ce5eb30903663dcc0a5148da80
khalia <khaliaparris01@gmail.com>
2021-11-16 15:45:13 +0000

    finished code challenge

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //write your code here
+function replaceWithYerr (array,start = 0, end = array.length -1){
+   let replaced = [];
+    for (let i in array){
+        if (i >= start && i < start + end){
+            replaced.push( "yerr" );
+        }else {
+            replaced.push(array[i]);
+        }
+    }
+    return replaced;
+}
+const months = ['Jan', 'February', 'March', 'April', 'May', 'June'];
+console.log(replaceWithYerr(months,1,3));
